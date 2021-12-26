929d1417c93d7804aba88db147b9fe468ef8b99d
Guruprasanna02 <guruprasannavasudevan02.com>
2021-12-26 14:45:38 +0530

    add: structs for insert

--- src/models.rs
+++ src/models.rs
@@ -1,9 +1,19 @@
+use super::schema::*;
+
 #[derive(Queryable)]
 pub struct AttackType {
     pub id: i32,
     pub att_type: i32,
     pub attack_radius: i32,
     pub attack_damage: i32,
+}
+
+#[derive(Insertable)]
+#[table_name = "attack_type"]
+pub struct NewAttackType<'a> {
+    pub att_type: &'a i32,
+    pub attack_radius: &'a i32,
+    pub attack_damage: &'a i32,
 }
 
 #[derive(Queryable)]
@@ -17,6 +27,16 @@ pub struct AttackerPath {
     pub emp_time: i32,
 }
 
+#[derive(Insertable)]
+#[table_name = "attacker_path"]
+pub struct NewAttackerPath<'a> {
+    pub y_coord: &'a i32,
+    pub x_coord: &'a i32,
+    pub is_emp: &'a bool,
+    pub emp_type: &'a i32,
+    pub emp_time: &'a i32,
+}
+
 #[derive(Queryable)]
 pub struct BlockType {
     pub id: i32,
@@ -24,6 +44,15 @@ pub struct BlockType {
     pub width: i32,
     pub height: i32,
     pub revenue: i32,
+}
+
+#[derive(Insertable)]
+#[table_name = "block_type"]
+pub struct NewBlockType<'a> {
+    pub name: &'a str,
+    pub width: &'a i32,
+    pub height: &'a i32,
+    pub revenue: &'a i32,
 }
 
 #[derive(Queryable)]
@@ -36,11 +65,25 @@ pub struct Game {
     pub defend_score: i32,
 }
 
+#[derive(Insertable)]
+#[table_name = "game"]
+pub struct NewGame<'a> {
+    pub attack_score: &'a i32,
+    pub defend_score: &'a i32,
+}
+
 #[derive(Queryable)]
 pub struct LevelsFixture {
     pub id: i32,
     pub start_date: i32,
     pub end_date: i32,
+}
+
+#[derive(Insertable)]
+#[table_name = "levels_fixture"]
+pub struct NewLevelFixture<'a> {
+    pub start_date: &'a i32,
+    pub end_date: &'a i32,
 }
 
 #[derive(Queryable)]
@@ -50,6 +93,12 @@ pub struct MapLayout {
     pub level_id: i32,
 }
 
+#[derive(Insertable)]
+#[table_name = "map_layout"]
+pub struct NewMapLayout<'a> {
+    pub player: &'a i32,
+}
+
 #[derive(Queryable)]
 pub struct MapSpaces {
     pub id: i32,
@@ -57,6 +106,13 @@ pub struct MapSpaces {
     pub blk_id: i32,
     pub x_coordinate: i32,
     pub y_coordinate: i32,
+}
+
+#[derive(Insertable)]
+#[table_name = "map_spaces"]
+pub struct NewMapSpaces<'a> {
+    pub y_coordinate: &'a i32,
+    pub x_coordinate: &'a i32,
 }
 
 #[derive(Queryable)]
@@ -69,6 +125,16 @@ pub struct ShortestPath {
     pub pathlist: String,
 }
 
+#[derive(Insertable)]
+#[table_name = "shortest_path"]
+pub struct NewShortestPath<'a> {
+    pub source_x: &'a i32,
+    pub source_y: &'a i32,
+    pub dest_x: &'a i32,
+    pub dest_y: &'a i32,
+    pub pathlist: &'a str,
+}
+
 #[derive(Queryable)]
 pub struct User {
     pub id: i32,
@@ -76,7 +142,21 @@ pub struct User {
     pub email: String,
     pub phone: String,
     pub username: String,
+    pub overall_rating: i32,
     pub is_pragyan: bool,
     pub password: String,
     pub is_verified: bool,
+}
+
+#[derive(Insertable)]
+#[table_name = "user"]
+pub struct NewUser<'a> {
+    pub name: &'a str,
+    pub email: &'a str,
+    pub phone: &'a str,
+    pub username: &'a str,
+    pub overall_rating: &'a i32,
+    pub is_pragyan: &'a bool,
+    pub password: &'a str,
+    pub is_verified: &'a bool,
 }
