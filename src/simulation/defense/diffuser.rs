3779aa992c35d779a67052402e6ed6b14dabd831
Venkatesh A <blackyvenky21@gmail.com>
2023-01-06 14:29:29 +0530

    feat(mine type simulation): added simulation for mines

--- src/simulation/defense/diffuser.rs
+++ src/simulation/defense/diffuser.rs
@@ -1,24 +1,41 @@
+use crate::models::*;
 pub struct Diffuser {
     pub id: i32,
     pub diffuser_type: i32,
     pub radius: i32,
     pub speed: i32,
-    pub damage: i32,
+    // pub damage: i32,
     pub x_position: i32,
     pub y_position: i32,
     pub is_alive: bool,
     pub target_emp_id: Option<i32>,
 }
 
+#[allow(dead_code)]
+pub struct Diffusers(Vec<Diffuser>);
+
 impl Diffuser {
-    // #[allow(dead_code)]
-    // pub fn new() -> Self {
-    //     println!("Diffuser Created");
-    //     todo!()
-    // }
+    #[allow(dead_code)]
+    pub fn new(diffuser_type: &DiffuserType, id: i32, x_position: i32, y_position: i32) -> Self {
+        Self {
+            id,
+            diffuser_type: diffuser_type.id,
+            radius: diffuser_type.radius,
+            speed: diffuser_type.speed,
+            // damage:diffuser_type.damage,
+            x_position,
+            y_position,
+            is_alive: true,
+            target_emp_id: None,
+        }
+    }
 
     #[allow(dead_code)]
     pub fn simulate() {
-        todo!()
+        //get list of emps within radius
+
+        //choose one emp optimally
+
+        //deacticate  it once used
     }
 }
--- src/simulation/defense/mine.rs
+++ src/simulation/defense/mine.rs
@@ -1,21 +1,74 @@
+use crate::models::*;
+use crate::simulation::attack::AttackManager;
+use anyhow::{Ok, Result};
+use diesel::dsl::not;
+use diesel::prelude::*;
+use diesel::PgConnection;
+
 pub struct Mine {
     pub id: i32,
     pub mine_type: i32,
     pub damage: i32,
     pub radius: i32,
-    pub is_activated: i32,
+    pub is_activated: bool,
     pub x_position: i32,
     pub y_position: i32,
 }
 
-impl Mine {
-    // #[allow(dead_code)]
-    // pub fn new() -> Self {
-    //     todo!()
-    // }
+pub struct Mines(Vec<Mine>);
+
+impl Mines {
+    #[allow(dead_code)]
+    pub fn new(conn: &mut PgConnection) -> Result<Self> {
+        use crate::schema::{building_type, map_spaces, mine_type};
+
+        let joined_table = map_spaces::table
+            .inner_join(building_type::table)
+            .inner_join(mine_type::table.on(not(building_type::mine_type.is_null())));
+
+        let mines: Vec<Mine> = joined_table
+            .load::<(MapSpaces, BuildingType, MineType)>(conn)?
+            .into_iter()
+            .map(|(map_space, _, mine_type)| Mine {
+                id: map_space.id,
+                mine_type: mine_type.id,
+                damage: mine_type.damage,
+                radius: mine_type.radius,
+                is_activated: true,
+                x_position: map_space.x_coordinate,
+                y_position: map_space.y_coordinate,
+            })
+            .collect();
+
+        Ok(Mines(mines))
+    }
 
     #[allow(dead_code)]
-    pub fn simulate() {
-        todo!()
+    pub fn simulate(&mut self, attack_manager: &mut AttackManager) -> Result<()> {
+        //get pos of attckers
+        let Mines(mines) = self;
+        let attackers = &mut attack_manager.attackers;
+
+        for (_, (_, attacker)) in attackers.iter_mut().enumerate() {
+            let (attacker_x, attacker_y) = attacker.get_current_position()?;
+
+            for mine in mines.iter_mut() {
+                let mine_x = mine.x_position;
+                let mine_y = mine.y_position;
+
+                let dist =
+                    (((mine_x - attacker_x).pow(2) + (mine_y - attacker_y).pow(2)) as f32).sqrt();
+
+                //check if there is any attacker within the range
+                if dist as i32 <= mine.radius {
+                    //damage attckers
+                    attacker.get_damage(mine.damage);
+                    //deativate mine
+                    mine.is_activated = false;
+                }
+            }
+        }
+
+        Ok(())
     }
 }
