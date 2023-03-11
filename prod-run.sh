b9a5f0fd9f0acdd35df1efc6a089da0057b38ce0
Guruprasanna02 <guruprasannavasudevan02@gmail.com>
2023-03-12 04:20:40 +0530

    fix: optimize prod run

--- prod-run.sh
+++ prod-run.sh
@@ -2,6 +2,9 @@
 
 WORKERS=1
 
+UP_W1=("docker" "compose" "-f" "docker-compose.yaml" "-f" "docker-compose.override.yaml" "-f" "docker-compose.prod.yaml" "up" "-d")
+UP_WM=("docker" "compose" "-f" "docker-compose.yaml" "-f" "docker-compose.prod.yaml" "--profile" "nginx" "up" "--scale" "server=${WORKERS}" "-d")
+
 if [ -z $1 ]; then
 	echo "Invalid command $1"
 	exit 1
@@ -9,17 +12,18 @@ if [ -z $1 ]; then
 else 
 	if [ $1 == "up" ]; then
 		if [ -z $2 ]; then
-			docker compose -f docker-compose.yaml -f docker-compose.prod.yaml up --scale server="${WORKERS}" -d
+			[[ ${WORKERS} == 1 ]] && ${UP_W1[@]} || ${UP_WM[@]}
+
 		else
 			if [ $2 == "build" ]; then
-				docker compose -f docker-compose.yaml -f docker-compose.prod.yaml up --build --scale server="${WORKERS}" -d
+				[[ ${WORKERS} == 1 ]] && ${UP_W1[@]} "--build" || ${UP_WM[@]} "--build"
 			else
 				echo "Invalid command $2"
 				exit 1
 			fi
 		fi
 	elif [ $1 == "down" ]; then
-		docker compose -f docker-compose.yaml -f docker-compose.prod.yaml down
+		docker compose -f docker-compose.yaml -f docker-compose.prod.yaml --profile nginx down
 	elif [ $1 == "logs" ]; then
 		docker compose -f docker-compose.yaml -f docker-compose.prod.yaml logs -f
 	else
