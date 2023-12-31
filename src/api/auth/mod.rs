45285b01fdd75f9d63d65369ba4101202cbe2340
khadeer <itskhadeerahmed@gmail.com>
2023-12-31 12:48:53 +0530

    WIP: google oauth2 access token generation

--- src/api/auth/mod.rs
+++ src/api/auth/mod.rs
@@ -2,29 +2,28 @@ use self::pragyan::PragyanMessage;
 use super::{PgPool, RedisPool};
 use crate::api::error;
 use actix_session::Session;
-use actix_web::error::ErrorUnauthorized;
-use actix_web::web::{self, Data, Json};
+use actix_web::error::{ErrorBadRequest, ErrorUnauthorized};
+use actix_web::web::{self, Data, Json, Query};
 use actix_web::Responder;
 use actix_web::{HttpResponse, Result};
-use oauth2::basic::BasicClient;
-use pwhash::bcrypt;
-use reqwest::header::LOCATION;
-use serde::{Deserialize, Serialize};
-
+use oauth2::reqwest::http_client;
+use oauth2::{basic::BasicClient, TokenResponse};
 use oauth2::{
-    AuthUrl, ClientId, ClientSecret, CsrfToken, PkceCodeChallenge, RedirectUrl, RevocationUrl,
-    Scope, TokenUrl,
+    AuthUrl, AuthorizationCode, ClientId, ClientSecret, CsrfToken, RedirectUrl, Scope, TokenUrl,
 };
-use std::env;
+use pwhash::bcrypt;
+use serde::{Deserialize, Serialize};
+// use std::env;
 mod pragyan;
 pub mod session;
 mod util;
 
 pub fn routes(cfg: &mut web::ServiceConfig) {
-    cfg.service(web::resource("/login").route(web::post().to(login)))
+    cfg.service(web::resource("/gauth2/login").route(web::get().to(google_login)))
+        .service(web::resource("/login").route(web::post().to(login)))
         .service(web::resource("/logout").route(web::post().to(logout)))
-        .service(web::resource("/gauth2/login").route(web::post().to(google_login)));
-}
+        .service(web::resource("/gauth2/callback").route(web::get().to(login_callback)));
+    }
 
 #[derive(Deserialize)]
 struct LoginRequest {
@@ -43,59 +42,77 @@ pub struct LoginResponse {
     pub email: String,
 }
 
-async fn google_login() -> impl Responder {
+#[derive(Debug, Deserialize)]
+pub struct QueryCode {
+    pub state: String,
+    pub code: String,
+}
+
+#[derive(Debug, Serialize)]
+pub struct GoogleLoginResponse {
+    pub authorize_url: String,
+    pub csrf_state: String,
+}
+
+fn client() -> BasicClient {
     let google_client_id = ClientId::new(
-        env::var("GOOGLE_CLIENT_ID").expect("Missing the GOOGLE_CLIENT_ID environment variable."),
+        "684397563262-1mp4uefnhlb6kbpobl5rdbnd336avkps.apps.googleusercontent.com".to_string(),
     );
-    let google_client_secret = ClientSecret::new(
-        env::var("GOOGLE_CLIENT_SECRET")
-            .expect("Missing the GOOGLE_CLIENT_SECRET environment variable."),
-    );
+    let google_client_secret = ClientSecret::new("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".to_string());
     let auth_url = AuthUrl::new("https://accounts.google.com/o/oauth2/v2/auth".to_string())
         .expect("Invalid authorization endpoint URL");
     let token_url = TokenUrl::new("https://www.googleapis.com/oauth2/v3/token".to_string())
         .expect("Invalid token endpoint URL");
 
     // Set up the config for the Google OAuth2 process.
-    let client = BasicClient::new(
+
+    BasicClient::new(
         google_client_id,
         Some(google_client_secret),
         auth_url,
         Some(token_url),
     )
-    // This example will be running its own server at localhost:8080.
-    // See below for the server implementation.
     .set_redirect_uri(
-        RedirectUrl::new("http://localhost:8080".to_string()).expect("Invalid redirect URL"),
+        RedirectUrl::new("http://localhost:8000/user/gauth2/callback".to_string())
+            .expect("Invalid redirect URL"),
     )
-    // Google supports OAuth 2.0 Token Revocation (RFC-7009)
-    .set_revocation_uri(
-        RevocationUrl::new("https://oauth2.googleapis.com/revoke".to_string())
-            .expect("Invalid revocation endpoint URL"),
-    );
+}
 
-    // Google supports Proof Key for Code Exchange (PKCE - https://oauth.net/2/pkce/).
-    // Create a PKCE code verifier and SHA-256 encode it as a code challenge.
-    let (pkce_code_challenge, _pkce_code_verifier) = PkceCodeChallenge::new_random_sha256();
-
+async fn google_login() -> impl Responder {
     // Generate the authorization URL to which we'll redirect the user.
-    let (authorize_url, _csrf_state) = client
+    let (authorize_url, csrf_token) = client()
         .authorize_url(CsrfToken::new_random)
-        // This example is requesting access to the "calendar" features and the user's profile.
-        .add_scope(Scope::new(
-            "https://www.googleapis.com/auth/calendar".to_string(),
-        ))
-        .add_scope(Scope::new(
-            "https://www.googleapis.com/auth/plus.me".to_string(),
-        ))
-        .set_pkce_challenge(pkce_code_challenge)
+        .add_scope(Scope::new("email".to_string()))
+        .add_scope(Scope::new("openid".to_string()))
+        .add_scope(Scope::new("profile".to_string()))
         .url();
 
-    let mut response = HttpResponse::Found();
-    response.append_header((LOCATION, authorize_url.to_string()));
-    response.body("");
-    response.finish()
+    return Json(GoogleLoginResponse {
+        authorize_url: authorize_url.to_string(),
+        csrf_state: csrf_token.secret().clone().to_string(),
+    });
 }
+
+async fn login_callback(params: Query<QueryCode>) -> impl Responder {
+    let code = AuthorizationCode::new(params.code.clone());
+    let token_result = client().exchange_code(code.clone()).request(http_client);
+    Json(CallbackResponse {
+        autherization_code: code.secret().clone().to_string(),
+        access_token: token_result
+            .unwrap()
+            .access_token()
+            .secret()
+            .clone()
+            .to_string(),
+    })
+}
+
+#[derive(Serialize)]
+pub struct CallbackResponse {
+    pub access_token: String,
+    pub autherization_code: String,
+}
+
 async fn login(
     request: web::Json<LoginRequest>,
     session: Session,
